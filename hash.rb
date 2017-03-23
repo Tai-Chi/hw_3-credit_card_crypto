2f0fd19bed082f7c13efaf5e12a93633f5d3bb2c
Dennis Hsieh <denny0530@gmail.com>
2017-03-23 21:46:49 +0800

    Need to solve the input problem to merge with Credit car & the to_s.hash not working normally

--- hash.rb
+++ hash.rb
@@ -1,16 +1,19 @@
+
+
 require 'rbnacl/libsodium'
 
-module HASH
-public
+def hash(file)
+  a = 'technical#{file}'
+  puts a.to_s.hash
+end
+
 def secure_hash(file)
-  #"blob #{file.size}\0#{file}"
-  h = RbNacl1::Hash.sha256(file)
+  print (RbNaCl::Hash.sha256(file)).bytes
 end
 
-##test
-msg = 'Hello World!'
+msg = 'Hello, World!\n'
+
+puts "Normal Hash"
+hash(msg)
+puts "Secure Hash!"
 secure_hash(msg)
-
-##Shall I need to Override it? 
-
-end
